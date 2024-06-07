463d58a5aab9ea3991b107b06f076c55537d2340
ReDTrBoy_ <109879756+ReDTrBoy@users.noreply.github.com>
2024-06-07 10:32:30 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -197,4 +197,110 @@ if (-not $spotifyYuklendi -or $UpdateSpotify -or $desteklenmeyenClientSurumu)
   }
   New-Item -Path $spotifyDizini -ItemType:Directory -Force | Write-Verbose
 
-  [System.Security.Principal.WindowsPrincipal] $principal = [System.Security.Principal.WindowsIdentity]::GetCurrent
+  [System.Security.Principal.WindowsPrincipal] $principal = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+  $kullaniciAdminMi = $principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+  Write-Host 'Kurulum baslatiliyor...'
+  if ($kullaniciAdminMi)
+  {
+    Write-Host
+    Write-Host 'Zamanlanmis gorev olusturuluyor...'
+    $uygulamaYolu = 'powershell.exe'
+    $gorevAdi = 'Spotify kurulum'
+    $aksiyon = New-ScheduledTaskAction -Execute $uygulamaYolu -Argument "-NoLogo -NoProfile -Command & `'$spotifyKurulumDosyaYolu`'"
+    $tetikleyici = New-ScheduledTaskTrigger -Once -At (Get-Date)
+    $ayarlar = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -WakeToRun
+    Register-ScheduledTask -Action $aksiyon -Trigger $tetikleyici -TaskName $gorevAdi -Settings $ayarlar -Force | Write-Verbose
+    Write-Host 'Kurulum gorevi zamanlandi. Gorev baslatiliyor...'
+    Start-ScheduledTask -TaskName $gorevAdi
+    Start-Sleep -Seconds 2
+    Write-Host 'Gorev kaldiriliyor...'
+    Unregister-ScheduledTask -TaskName $gorevAdi -Confirm:$false
+    Start-Sleep -Seconds 2
+  }
+  else
+  {
+    Start-Process -FilePath "$spotifyKurulumDosyaYolu"
+  }
+
+  while ($null -eq (Get-Process -Name Spotify -ErrorAction SilentlyContinue))
+  {
+    # Kurulum tamamlanana kadar bekliyor
+    Start-Sleep -Milliseconds 100
+  }
+
+  Write-Host 'Spotify durduruluyor...Yeniden'
+
+  Stop-Process -Name Spotify
+  Stop-Process -Name SpotifyWebHelper
+  if ([Environment]::Is64BitOperatingSystem) { # Bilgisayarin 64-bit isletim sistemi calistirip calistirmadigini kontrol edin
+    Stop-Process -Name SpotifyFullSetupX64
+  } else {
+     Stop-Process -Name SpotifyFullSetup
+  }
+}
+
+Write-Host "En son yama (chrome_elf.zip) indiriliyor...`n"
+$elfYolu = Join-Path -Path $PWD -ChildPath 'chrome_elf.zip'
+try
+{
+  $baytlar = [System.IO.File]::ReadAllBytes($spotifyCalistirilabilir)
+  $peBasligi = [System.BitConverter]::ToUInt16($baytlar[0x3C..0x3D], 0)
+  $is64Bit = $baytlar[$peBasligi + 4] -eq 0x64
+
+  if ($is64Bit) {
+    $uri = 'https://github.com/mrpond/BlockTheSpot/releases/latest/download/chrome_elf.zip'
+  } else {
+    Write-Host 'Su anda, x86 mimarisi yeni bir guncelleme almadigi icin reklam engelleyici dogru calismayabilir.'
+    $uri = 'https://github.com/mrpond/BlockTheSpot/releases/download/2023.5.20.80/chrome_elf.zip'
+  }
+
+  DosyaAl -Uri $uri -HedefDosya "$elfYolu"
+}
+catch
+{
+  Write-Output $_
+  Start-Sleep
+}
+
+Expand-Archive -Force -LiteralPath "$elfYolu" -DestinationPath $PWD
+Remove-Item -LiteralPath "$elfYolu" -Force
+
+Write-Host 'Spotify yamaniyor...'
+$yamaDosyalari = (Join-Path -Path $PWD -ChildPath 'dpapi.dll'), (Join-Path -Path $PWD -ChildPath 'config.ini')
+
+Copy-Item -LiteralPath $yamaDosyalari -Destination "$spotifyDizini"
+
+function VcRedistKur {
+    # https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170
+    $vcRedistX86Url = "https://aka.ms/vs/17/release/vc_redist.x86.exe"
+    $vcRedistX64Url = "https://aka.ms/vs/17/release/vc_redist.x64.exe"
+
+    if ([Environment]::Is64BitOperatingSystem) {
+        if (!(Test-Path 'HKLM:\Software\Microsoft\VisualStudio\14.0\VC\Runtimes\x64')) {
+            $vcRedistX64Dosya = Join-Path -Path $PWD -ChildPath 'vc_redist.x64.exe'
+            Write-Host "vc_redist.x64.exe indiriliyor ve kuruluyor..."
+            DosyaAl -Uri $vcRedistX64Url -HedefDosya $vcRedistX64Dosya
+            Start-Process -FilePath $vcRedistX64Dosya -ArgumentList "/install /quiet /norestart" -Wait
+        }
+    }
+    else {
+        if (!(Test-Path 'HKLM:\Software\Microsoft\VisualStudio\14.0\VC\Runtimes\x86')) {
+            $vcRedistX86Dosya = Join-Path -Path $PWD -ChildPath 'vc_redist.x86.exe'
+            Write-Host "vc_redist.x86.exe indiriliyor ve kuruluyor..."
+            DosyaAl -Uri $vcRedistX86Url -HedefDosya $vcRedistX86Dosya
+            Start-Process -FilePath $vcRedistX86Dosya -ArgumentList "/install /quiet /norestart" -Wait
+        }
+    }
+}
+
+VcRedistKur
+
+$tempDizin = $PWD
+Pop-Location
+
+Remove-Item -LiteralPath $tempDizin -Recurse
+
+Write-Host 'Yama Islemi Tamamlandi, Spotify baslatiliyor...'
+
+Start-Process -WorkingDirectory $spotifyDizini -FilePath $spotifyCalistirilabilir
+Write-Host 'Bitti.'
